d0b17d5df442c5bc2697c94b21dc66cea3b75a8a
Hoa Nguyen <hoowahwin@gmail.com>
2023-06-07 23:06:22 -0500

    nuts

--- components/Card.jsx
+++ components/Card.jsx
@@ -15,10 +15,7 @@ const Card = (props) => {
         </div>
         <div className="text p-6">
           <h1 className="text-xl py-5 dark:text-gray-300">{cardTitle}</h1>
-          <p className="text-sm leading-7 text-neutral-800 dark:text-neutral-400">
-            {description}
-            {id}
-          </p>
+          <p className="text-sm leading-7 text-neutral-800 dark:text-neutral-400">{description}</p>
           <div className="text-center bg-gradient-to-r from-green-500 to-emerald-500 text-gray-100 rounded-lg hover:to-green-500 hover:from-emerald-500 duration-500 my-10 md:mx-10 px-4 py-2 hover:scale-110">
             <Link href={"/" + id}>{buttonMessage}</Link>
           </div>
--- components/CardDetails.jsx
+++ components/CardDetails.jsx
@@ -11,7 +11,7 @@ export const CARD_DETAILS = [
     id: "Art",
     cardTitle: "check out my art!",
     description:
-      "Along with my passion for coding, I believe that having a hobby, such as creating art, is important for a balanced life. I use art as a way to express myself and to relax away from the computer.",
+      "Along with my passion for coding, I believe that having a hobby, such as creating art, is important for a balanced life. I use art as a way to express myself and to relax away from the keyboard. I love that I can create something that myself and others can enjoy.",
     cardImage: "../art.jpg",
     buttonMessage: "my art gallery ->",
   },
@@ -19,7 +19,7 @@ export const CARD_DETAILS = [
     id: "Tech",
     cardTitle: "technologies im learning:",
     description:
-      "I believe that supplementing my education with self-taught skills is important. Versitility is one of the ways I hone my skills as a developer/analyst. Besides, it's fun to learn new things!",
+      "I believe that supplementing my education with self-taught skills is important. Whether it be a new framework or a Python package, I'm open to learning it. Versitility is one of the ways I hone my skills as a developer/analyst. Besides, it's fun to learn new things!",
     cardImage: "../coding.jpg",
     buttonMessage: "my tools ->",
   },
